71587ce23721c0eca15788cc4780b3e788e4bbaa
Justin Williams <justin@secondgearsoftware.com>
2012-09-23 10:51:02 -0700

    Cleaned up formatting on NSData+SGAdditions.h

--- Foundation/NSData+SGAdditions.m
+++ Foundation/NSData+SGAdditions.m
@@ -13,58 +13,60 @@
 
 - (id)initWithBase64String:(NSString *)theString
 {
-  self = [NSData dataFromBase64String:theString];
-  return self;
+    self = [NSData dataFromBase64String:theString];
+    return self;
 }
-
 
 - (NSData *)hmacSHA1DataValueWithKey:(NSData *)keyData;
 {
-  void* buffer = malloc(CC_SHA1_DIGEST_LENGTH);
-  CCHmac(kCCHmacAlgSHA1, [keyData bytes], [keyData length], [self bytes], [self length], buffer);
-  return [NSData dataWithBytesNoCopy:buffer length:CC_SHA1_DIGEST_LENGTH freeWhenDone:YES];
+    void* buffer = malloc(CC_SHA1_DIGEST_LENGTH);
+    CCHmac(kCCHmacAlgSHA1, [keyData bytes], [keyData length], [self bytes], [self length], buffer);
+    return [NSData dataWithBytesNoCopy:buffer length:CC_SHA1_DIGEST_LENGTH freeWhenDone:YES];
 }
 
 - (NSString *)UTF8String
 {
-  return [[NSString alloc] initWithBytes:[self bytes] length:[self length] encoding:NSUTF8StringEncoding];
+    return [[NSString alloc] initWithBytes:[self bytes] length:[self length] encoding:NSUTF8StringEncoding];
 }
 
 @end
 
-@implementation NSMutableData (STAdditions)
+@implementation NSMutableData (SGAdditions)
 
 - (void)appendUTF8StringWithFormat:(NSString *)inString, ...;
 {
-  va_list args;
-  va_start(args, inString);
-  
-  NSString *formattedString = [[NSString alloc] initWithFormat:inString arguments:args];
-  [self appendUTF8String:formattedString];
-  
-  va_end(args);
+    va_list args;
+    va_start(args, inString);
+    
+    NSString *formattedString = [[NSString alloc] initWithFormat:inString arguments:args];
+    [self appendUTF8String:formattedString];
+    
+    va_end(args);
 }
 
-- (void)appendUTF8String:(NSString *)inString;
+- (void)appendUTF8String:(NSString *)string;
 {
-  [self appendString:inString withEncoding:NSUTF8StringEncoding];
+    [self appendString:string withEncoding:NSUTF8StringEncoding];
 }
 
-- (void)appendString:(NSString *)inString withEncoding:(NSStringEncoding)inEncoding;
+- (void)appendString:(NSString *)string withEncoding:(NSStringEncoding)inEncoding;
 {
-  NSUInteger byteLength = [inString lengthOfBytesUsingEncoding:inEncoding];
-  
-  if (!byteLength) {
-    return;
-  }
-  
-  char *buffer = malloc(byteLength);
-  
-  if ([inString getBytes:buffer maxLength:byteLength usedLength:NULL encoding:inEncoding options:NSStringEncodingConversionExternalRepresentation range:NSMakeRange(0,byteLength) remainingRange:NULL]) {
-    [self appendBytes:buffer length:byteLength];
-  }
-  
-  free(buffer);
+    NSUInteger byteLength = [string lengthOfBytesUsingEncoding:inEncoding];
+    
+    if (!byteLength)
+    {
+        return;
+    }
+    
+    char *buffer = malloc(byteLength);
+    
+    BOOL successfullyGotBytes = [string getBytes:buffer maxLength:byteLength usedLength:NULL encoding:inEncoding options:NSStringEncodingConversionExternalRepresentation range:NSMakeRange(0,byteLength) remainingRange:NULL];
+    if (successfullyGotBytes == YES)
+    {
+        [self appendBytes:buffer length:byteLength];
+    }
+    
+    free(buffer);
 }
 
 @end
