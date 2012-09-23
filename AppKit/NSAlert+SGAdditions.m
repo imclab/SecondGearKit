a08e8453c715b2f737e9dc8688a094ac7830bd1f
Justin Williams <justin@secondgearsoftware.com>
2012-09-23 10:48:11 -0700

    Updated NSAlert+SGAdditions to ARC.

--- AppKit/NSAlert+SGAdditions.m
+++ AppKit/NSAlert+SGAdditions.m
@@ -9,50 +9,35 @@
 #import "NSAlert+SGAdditions.h"
 
 @interface SGAlertSheetCompletionHandlerRunner : NSObject
-@property (nonatomic, retain) NSAlert *alert;
+@property (nonatomic, strong) NSAlert *alert;
 @property (nonatomic, copy) SGAlertSheetCompletionHandler completionHandler;
 @end
 
 @implementation SGAlertSheetCompletionHandlerRunner
 
-@synthesize alert;
-@synthesize completionHandler;
-
 - initWithAlert:(NSAlert *)theAlert completionHandler:(SGAlertSheetCompletionHandler)theHandler
 {
   if ((self = [super init]))
   {  
-    alert = [theAlert retain];
-    completionHandler = [theHandler copy];
+    _alert = [theAlert retain];
+    _completionHandler = [theHandler copy];
   }
   
   return self;
 }
 
-- (void)dealloc;
-{
-  [alert release];
-  [completionHandler release];
-  [super dealloc];
-}
-
 - (void)startOnWindow:(NSWindow *)parentWindow;
 {
-  // We have to live until the callback, but a -retain will annoy clang-sa.
-	[self performSelector:@selector(retain)];
   [self.alert beginSheetModalForWindow:parentWindow modalDelegate:self didEndSelector:@selector(alertDidEnd:returnCode:contextInfo:) contextInfo:NULL];
 }
 
 - (void)alertDidEnd:(NSAlert *)theAlert returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo;
 {
-  NSAssert(theAlert == alert, @"Got a alert different from what I expected -- This should never happen");
-  
-  // Clean up the hidden -retain from -startOnWindow:, first and with -autorelease in case the block asplodes.
-  [self performSelector:@selector(autorelease)];
-  
+  NSAssert(theAlert == self.alert, @"Got a alert different from what I expected -- This should never happen");
+
   if (self.completionHandler != nil)
   {
-    self.completionHandler(alert, returnCode);
+    self.completionHandler(self.alert, returnCode);
   }
 }
 
@@ -65,7 +50,6 @@
 {
   SGAlertSheetCompletionHandlerRunner *runner = [[SGAlertSheetCompletionHandlerRunner alloc] initWithAlert:self completionHandler:completionHandler];
   [runner startOnWindow:window];
-  [runner release];
 }
 
 @end
