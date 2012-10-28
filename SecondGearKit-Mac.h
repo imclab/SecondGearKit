07ef3f8cf058413a92f84b4f7891f1be4693df34
Justin Williams <justin@secondgearsoftware.com>
2012-10-28 11:35:35 -0500

    Updated the include file for the Mac framework to include the new Foundation stuff.

--- SecondGearKit-Mac.h
+++ SecondGearKit-Mac.h
@@ -9,22 +9,22 @@
 #import <Foundation/Foundation.h>
 
 // External
+#import "Reachability.h"
+#import "SGKeychain.h"
+#import "SGMimiMailer.h"
 #import "NSData+Base64.h"
-#import "EMKeychainItem.h"
 
 // Foundation
+#import "NSArray+SGAdditions.h"
 #import "NSData+SGAdditions.h"
 #import "NSDate+SGAdditions.h"
 #import "NSDictionary+SGAdditions.h"
 #import "NSMutableString+SGAdditions.h"
 #import "NSObject+SGAdditions.h"
 #import "NSString+SGAdditions.h"
-#import "NSString+UUID.h"
 #import "NSURL+SGAdditions.h"
-#import "NSArray+SGAdditions.h"
+#import "NSSet+SGAdditions.h"
 
-// SGHTTPRequest
-#import "SGHTTPRequest.h"
 
 // AppKit
 #import "NSAlert+SGAdditions.h"
